61d66faf0cc77ab76abbc2435475ed5047abc15c
Yajindra <yajindragtm@gmail.com>
2021-09-23 15:16:15 +0545

    Data Types In Js

---
+++ data-types.js
@@ -0,0 +1,24 @@
+// DATA TYPES IN NODE - JS
+/* 
+ 1. OBJECT
+ 2. ARRAY
+ 3. NUMBER
+ 4. BOOLEAN
+ 5. FLOAT
+ 6. DATETIME
+ 7. NULL
+ 9. UNDEFINED
+*/
+
+// EXAMPLE OF DATATYPES
+let a = 1; // --- Number
+let b = 'Hello World' // --- String
+let c  = true; // --- Boolen
+let d = 1.5; //--- Float
+
+// CHECK DATA TYPES OF VALIABLE USING typeOf() Method
+console.log(typeof a);
+console.log( typeof b);
+console.log(typeof c);
+console.log(typeof c);
+console.log(typeof d);
