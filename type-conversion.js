566e2cc2cb4ae264cc559cabe9f52a29d96154ce
Yajindra <yajindragtm@gmail.com>
2021-09-28 13:49:55 +0545

    Type Conversion in js

---
+++ type-conversion.js
@@ -0,0 +1,33 @@
+let val;
+
+
+// Number to String
+val = String(555);
+val = String(4+4);
+// BOOLEAN to STRING
+val = String(true);
+// DATE to STRING
+val = String(new Date());
+// ARRAY to STRING
+val = String(['yaji','eliza','js'])
+
+// toStrig()
+val =(5).toString();
+val = (true).toString();
+
+// STRING TO NUMBER
+val = Number('5')
+// BOOLEAN to Number
+val = Number(true);
+val = Number(false);
+val = Number(null);
+
+// parseInt() - return intiger no decimal
+val = parseInt('100');
+val = parseFloat('100.30'); // Return float with decimal
+
+// Output
+console.log(val);
+console.log(typeof val);
+//console.log(val.length);
+
